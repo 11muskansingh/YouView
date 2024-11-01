d629447abfbb69784b8936c3a8b9af744cc5b9bb
Muskan Singh <144880038+11muskansingh@users.noreply.github.com>
2024-11-01 20:05:30 +0530

    Update AxiosInstance.jsx

--- src/utils/AxiosInstance.jsx
+++ src/utils/AxiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const accessToken = localStorage.getItem("accessToken");
 const axiosInstance = axios.create({
   baseURL:
-    "https://video-stream-i3lribuew-11muskansinghs-projects.vercel.app/api/v1",
+    "https://video-stream-6378kgkfa-11muskansinghs-projects.vercel.app/api/v1",
   withCredentials: true,
 });
 
