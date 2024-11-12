91fceabc95147dda3ba7c4f6112818e96ba2b117
Muskan Singh <144880038+11muskansingh@users.noreply.github.com>
2024-11-12 16:38:36 +0530

    Update AxiosInstance.jsx

--- src/utils/AxiosInstance.jsx
+++ src/utils/AxiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const accessToken = localStorage.getItem("accessToken");
 const axiosInstance = axios.create({
   baseURL:
-    "https://video-stream-alpha-five.vercel.app/api/v1",
+    "https://videostream-backend-fhts.onrender.com/api/v1",
      withCredentials: true,
 });
 
